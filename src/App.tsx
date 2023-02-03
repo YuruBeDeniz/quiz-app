e2ba3b94353f8654c7894db26cd81c4168ca8ba6
Deniz Saganak <saganak.deniz@gmail.com>
2023-02-03 13:44:24 +0100

    scaffolding the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { MouseEvent } from "react";
 
 function App() {
+  //the first function we fire off when we start the quiz:
+  const startTrivia = async () => {}
+
+  //we will trigger this function when the user selects an answer
+  //that's why the function will accept event as a parameter
+  const checkAnswer = (e: MouseEvent<HTMLButtonElement>) => {};
+  
+  //this function will be triggered when the user clicks for the next question:
+  const nextQuestion = () => {}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Quiz</h1>
+     <button className="start" onClick={startTrivia}>Start</button>
+     <p className="score">Score:</p>
+     <p>Loading questions...</p>
     </div>
   );
 }
 
 export default App;
+x
